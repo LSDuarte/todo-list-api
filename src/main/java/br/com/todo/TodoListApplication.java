e7ee3cb46911ee8ff0217cb12f660668dc5a68c8
Larissa Duarte <larissaduarte93@gmail.com>
2021-12-13 09:35:20 -0400

    Testando api

--- src/main/java/br/com/todo/TodoListApplication.java
+++ src/main/java/br/com/todo/TodoListApplication.java
@@ -2,9 +2,17 @@ package br.com.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TodoListApplication {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
