3904f8d677e69382abf7c2f622f9c06ee20dc4d5
Larissa Duarte <larissaduarte93@gmail.com>
2021-12-21 17:04:09 -0400

    Adicionando propriedade para comunicar com o front

--- src/main/java/br/com/todo/rest/TodoController.java
+++ src/main/java/br/com/todo/rest/TodoController.java
@@ -2,6 +2,7 @@ package br.com.todo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import br.com.todo.repository.TodoRepository;
 
 @RestController
 @RequestMapping("/api/todos")
+@CrossOrigin("http://localhost:4200")
 public class TodoController {
 
 	@Autowired
