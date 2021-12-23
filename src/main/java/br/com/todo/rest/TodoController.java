3e0b3960ad147b33e0988cfb7525d48711a8107a
Larissa Duarte <larissaduarte93@gmail.com>
2021-12-23 15:52:14 -0400

    Implementando função done

--- src/main/java/br/com/todo/rest/TodoController.java
+++ src/main/java/br/com/todo/rest/TodoController.java
@@ -1,5 +1,6 @@
 package br.com.todo.rest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,6 +47,18 @@ public class TodoController {
 	@DeleteMapping("{id}")
 	public void delete(@PathVariable Long id) {
 		repository.deleteById(id);
+	}
+	
+	// http://localhost:8282/api/todos/1/done
+	@PatchMapping("{id}/done")
+	public Todo markAsDone(@PathVariable Long id) {
+		return repository.findById(id)
+						.map(todo -> {
+							todo.setDone(true);
+							todo.setDoneDate(LocalDateTime.now());
+							repository.save(todo);
+							return todo;
+						}).orElse(null);
 	}
 
 }
